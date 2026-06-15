ae97f7675cc077d3c1015ed65433478befe9700f
vena-media <venaempress@gmail.com>
2026-06-15 20:49:09 +0100

    review_form

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// const products = [
+//     { name: "Smartphone X" },
+//     { name: "Laptop Pro 15" },
+//     { name: "Wireless Headphones" },
+//     { name: "Smart Watch" }
+// ];
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const select = document.getElementById("product");
+
+    if (select) {
+        products.forEach(product => {
+            const option = document.createElement("option");
+            option.value = product.name;
+            option.textContent = product.name;
+            select.appendChild(option);
+        });
+    }
+
+
+    const counterEl = document.getElementById("counter");
+
+    if (counterEl) {
+        let count = localStorage.getItem("reviewCount") || 0;
+        count = Number(count) + 1;
+
+        localStorage.setItem("reviewCount", count);
+        counterEl.textContent = count;
+    }
+});
